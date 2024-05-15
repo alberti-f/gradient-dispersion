6af1e94efcbf90688bfa8ac27de9c68ed9c71969
alberti-f <fnc.alberti@gmail.com>
2024-05-15 12:55:13 +0200

    adapted paths for sharing

--- settings.py
+++ settings.py
@@ -5,25 +5,43 @@
 import numpy as np
 import nibabel as nib
 import ciftools_FA as ct
-import pandas as pd
 
-# directory containing subdirectories named fter subject IDs that contain the timeseries and surface files
-root_dir = "/home/fralberti/Data/HCP_1200"
+
+
+# group name to prefix the output files
+group = "338" 
+
+# directory of HCP group average data
+group_dir = "/path/to/HCP_S1200_GroupAvg_v1"
+
+# directory containing the HCP subject directories
+subj_dir = "/path/to/subject/directories"
+
 # directory where all intermediate files and the final output will be saved
-output_dir = "/home/fralberti/Documents/BlackBox/Prj_Network-variability/Results"
+output_dir = f"/path/to/output/directory"
+
+# directory containing the GCCA results
+gcca_dir = f"/path/to/GCCA/results"
+
 # path to cognitive data
-cog_path = f'{root_dir}/unrestricted_fralberti_4_8_2022_8_22_23.csv'
+cog_path = "/path/to/unrestricted/data.csv"
+
+# path to restricted data
+rest_path = "/path/to/restricted/data.csv"
+
 # .txt containing IDs of subjects to include in the analysis
-f = open(f'{root_dir}/Subjects/subj_IDs_338.txt', 'r')
+f = open(f'{subj_dir}/subj_IDs_{group}.txt', 'r')
 subj_id = np.array(f.read().splitlines(), dtype='int32')
-# group name
-group = '338'
+
 
 # Schaefer atlas to use for analyses
 lbl_N = 400
 nw_N = 7
-networks_txt = f'/home/fralberti/Data/Shaefer2018_HCP/Schaefer2018_{lbl_N}Parcels_{nw_N}Networks_order_info.txt'
-networks = nib.load(f'/home/fralberti/Data/Shaefer2018_HCP/Schaefer2018_{lbl_N}Parcels_{nw_N}Networks_order.dlabel.nii')
+
+# Path to Shaefer2018 atlas registered to HCP
+networks_dir = f'/path/to/Shaefer2018_HCP'
+networks_txt = f'{networks_dir}/Schaefer2018_{lbl_N}Parcels_{nw_N}Networks_order_info.txt'
+networks = nib.load(f'{networks_dir}/Schaefer2018_{lbl_N}Parcels_{nw_N}Networks_order.dlabel.nii')
 NW_tbl = ct.agg_networks(networks, networks, func=np.median, by_hemisphere=False, label_tbl=True)[1]
 nw_name = NW_tbl.groupby('name').agg(np.median).sort_values('network').index.to_list()[1:]
 
