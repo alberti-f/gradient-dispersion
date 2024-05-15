761658a554acb06c58c4ec03aac77fbb890d81b0
alberti-f <fnc.alberti@gmail.com>
2024-05-15 15:07:27 +0200

    write log

--- run_analyses.sh
+++ run_analyses.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo -e "\n\n\n\ncifti_to_npy\n" && \
-python3 01.cifti_to_npy.py && \
-echo -e "\n\n\n\ninterindividual_dispersion\n" && \
-python3 02.interindividual_dispersion.py && \
-echo -e "\n\n\n\nvariability_clusters\n" && \
-python3 03.variability_clusters.py && \
-echo -e "\n\n\n\ngenerate_csv\n"  && \
-python3 04.generate_csv.py && \
-echo -e "\nmodeling_inteligence\n" && \
-python3 05.modeling_intelligence.py && \
-echo -e "\n\n\n\ngraph_analysis\n" && \
-python3 06.graph_analysis.py && \
-echo -e "\n\n\n\nmetric_maps\n" && \
-python3 07.metric_maps.py && \
-echo -e "\n\n\n\nDONE" > log.txt
+echo -e "\n\n\n\ncifti_to_npy\n" > log.txt && \
+python3 01.cifti_to_npy.py >> log.txt && \
+echo -e "\n\n\n\ninterindividual_dispersion\n" >> log.txt && \
+python3 02.interindividual_dispersion.py >> log.txt && \
+echo -e "\n\n\n\nvariability_clusters\n" >> log.txt && \
+python3 03.variability_clusters.py >> log.txt && \
+echo -e "\n\n\n\ngenerate_csv\n"  >> log.txt && \
+python3 04.generate_csv.py >> log.txt && \
+echo -e "\nmodeling_inteligence\n" >> log.txt && \
+python3 05.modeling_intelligence.py >> log.txt && \
+echo -e "\n\n\n\ngraph_analysis\n" >> log.txt && \
+python3 06.graph_analysis.py >> log.txt && \
+echo -e "\n\n\n\nmetric_maps\n" >> log.txt && \
+python3 07.metric_maps.py >> log.txt && \
+echo -e "\n\n\n\nDONE" >> log.txt 
